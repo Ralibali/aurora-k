1d90d1c2d76ec675c19c51ed41b2ea6f0ff70fcd
Ralibali <36996821+Ralibali@users.noreply.github.com>
2026-04-29 02:51:38 +0200

    Use SEO-optimized landing page V3

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { AdminShell } from "@/components/AdminLayout";
 import { DriverLayout } from "@/components/DriverLayout";
 import { PlatformAdminShell } from "@/components/PlatformAdminLayout";
 
-const LandingPage = lazy(() => import("./pages/LandingPageV2"));
+const LandingPage = lazy(() => import("./pages/LandingPageV3"));
 const TransportledningssystemPage = lazy(() => import("./pages/TransportledningssystemPage"));
 const CoredinationAlternativPage = lazy(() => import("./pages/CoredinationAlternativPage"));
 const TjansterPage = lazy(() => import("./pages/TjansterPage"));
