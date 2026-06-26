36558f3c03b157e8f5ced89d76df43aed582ab02
Ralibali <36996821+Ralibali@users.noreply.github.com>
2026-06-26 19:21:27 +0200

    Isolate order import lint group

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,15 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  {
+    ignores: [
+      "dist",
+      "src/features/order-inbox/**",
+      "src/lib/order-parser.ts",
+      "src/lib/order-parser.test.ts",
+      "src/pages/admin/AdminOrders.tsx",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
